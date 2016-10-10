78de208062d1e93fb1ebe64bdab30aed37f38d0f
Mandrace84 <martinomandelli84@googlemail.com>
2016-10-10 15:58:06 +0200

    Update upcamp.js

--- upcamp.js
+++ upcamp.js
@@ -19,61 +19,62 @@ var mailchimp = new Mailchimp(API_KEY);
 mailchimp.post ({
 	
 	path : '/campaigns'
-    body: Campdata[14] 
+        body: Campdata[14] 
 })
-    .then(function (result) {
+    
+.then(function (result) {
 		
-mailchimp.put ({
+   mailchimp.put ({
 	
 	path: '/campaigns/' + result.id + '/content'
 
 	body: { html : function contents () {
 	
-	fs.readFile('./templates/template.html', (err, data) => {
+	         fs.readFile('./templates/template.html', (err, data) => {
   
-    if (err) throw err;
+                               if (err) throw err;
   
-   var decoder = new StringDecoder('utf8');
+                               var decoder = new StringDecoder('utf8');
   
-   decoder.write (data); 
+                               decoder.write (data); 
   
-  var template = Handlebars.compile(decoder.write (data)); 
+                               var template = Handlebars.compile(decoder.write (data)); 
   
-  var risultato = template (Names)
+                               var risultato = template (Names)
   
   
-  });
+                               });
   
-  return risultato
+                  return risultato
   
-	};
+	          };
 	
-	};
+	          };
 	
-});	
+       })	
 	
-	.then(function (result) {
+       .then(function (result) {
 		
 		console.log ('contents created')
 	
-    })
-    .catch(function (err) {
+        })
+        .catch(function (err) {
 
 	console.log(err);
 
 	
-    })
+        })
 
 console.log ('campaign created')	
 
-	})
+})
 	
 
-    .catch(function (err) {
+.catch(function (err) {
 
 	
-	console.log(err); 
+console.log(err); 
 
 	
-    })
+})
 
